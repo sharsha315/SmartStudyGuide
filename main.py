e608c706330aaa537c855b12b367e952b6a4dd45
Harsha S <sharsha315@gmail.com>
2024-08-05 11:23:08 +0000

    Updated code for simple chat interface

--- main.py
+++ main.py
@@ -22,14 +22,29 @@ def generate_response(input_text):
     )
     st.info(chat.invoke(input_text).content)
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-with st.form("my_form"):
-    text = st.text_area(
-        "Enter text:",
-        "",
-    )
-    submitted = st.form_submit_button("Submit")
-    if not AI71_API_KEY.startswith("api71"):
-        st.warning("Please enter your AI71 API key!", icon="⚠")
-    if submitted and AI71_API_KEY.startswith("api71"):
-        generate_response(text)
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# with st.form("my_form"):
+#     text = st.text_area(
+#         "Enter text:",
+#         "",
+#     )
+#     submitted = st.form_submit_button("Submit")
+#     if not AI71_API_KEY.startswith("api71"):
+#         st.warning("Please enter your AI71 API key!", icon="⚠")
+#     if submitted and AI71_API_KEY.startswith("api71"):
+#         generate_response(text)
+
+if prompt := st.chat_input("Say something!"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(generate_response(prompt))
+    st.session_state.messages.append({"role": "assistant", "content": response})
